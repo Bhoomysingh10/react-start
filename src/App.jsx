b258bec90a70fd2c7f0199798c420eb6b3bd8d1f
Bhoomysingh10 <bhoomysingh123@gmail.com>
2024-03-14 18:50:36 +0530

    Started learning react.js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Acha from "./dusra"
+
+function App() {
+    return (
+      <>
+   <Acha/>
+   <h1>hello kaise hai ap log</h1>
+   </>
+  
+  )
+}
+
+export default App
+//jsx ek hi elemennt return aur export karta hai isliye humne pehle wla h1 tag delete kiya
+//agr  hume multiple cheeze deni ho toh hum ek div element bana sakte hai ya fragement (<>    </>)
---
+++ src/dusra.jsx
@@ -0,0 +1,7 @@
+function Acha (){
+
+    return(
+        <h1>ahmmmm dusra function | achaji</h1>
+    )
+}
+export default Acha
